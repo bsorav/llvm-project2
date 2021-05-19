0d328a45476e0efd07c2ff3df1e4c59946090009
Sorav Bansal <sbansal@iitd.ac.in>
2021-05-19 13:14:38 +0530

    DYN_DEBUG: do not return inside DYN_DEBUG macros
    We actually expect the body of the DYN_DEBUG macros to exit in a
    straight-line fashion, so we can execute the setting/unsetting of
    certain debug-related flags.  For disableSemanticAA, we were actually
    allowing "return" statements inside the body. Fixed that now.

--- llvm/lib/Analysis/SemanticAliasAnalysis.cpp
+++ llvm/lib/Analysis/SemanticAliasAnalysis.cpp
@@ -63,10 +63,17 @@ SemanticAAResult::alias(const MemoryLocation &LocA,
   string nameB = memory_location_get_name(LocB);
   DYN_DEBUG2(aliasAnalysis, std::cout << "SemanticAAResult::" << __func__ << " " << __LINE__ << ": LocA = " << nameA << "\n");
   DYN_DEBUG2(aliasAnalysis, std::cout << "SemanticAAResult::" << __func__ << " " << __LINE__ << ": LocB = " << nameB << "\n");
+
+  AliasResult retval;
+  bool should_return_immediately = false;
   DYN_DEBUG_MUTE(disableSemanticAA,
     // Forward the query to the next analysis.
-    return AAResultBase::alias(LocA, LocB, AAQI);
+    retval = AAResultBase::alias(LocA, LocB, AAQI);
+    should_return_immediately = true; //to avoid control flow from inside the macro, simply set a flag
   );
+  if (should_return_immediately) {
+    return retval;
+  }
 
   assert(notDifferentParent(LocA.Ptr, LocB.Ptr) &&
          "SemanticAliasAnalysis doesn't support interprocedural queries.");
@@ -115,10 +122,14 @@ SemanticAAWrapperPass::SemanticAAWrapperPass() : ImmutablePass(ID) {
 bool SemanticAAWrapperPass::doInitialization(Module &M)
 {
   DYN_DEBUG(aliasAnalysis, std::cout << "SemanticAAResult::doInitialization() called\n");
+  bool should_return_immediately = false;
   DYN_DEBUG_MUTE(disableSemanticAA,
     Result.reset(new SemanticAAResult(nullptr, nullptr));
-    return false
+    should_return_immediately = true; //to avoid control flow from inside the macro, simply set a flag
   );
+  if (should_return_immediately) {
+    return false;
+  }
   //string const& fname = F.getName().str();
   //if (m_function_tfg_map->count(fname)) {
   //  return false;
