86236a50fb18de70eeffc8e023aa150280af6016
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-09-27 16:57:56 +0530

    llvm2tfg: sym_exec_common::get_tfg_common(): setting argument regs: handle vararg correctly

--- llvm/lib/Analysis/Superopt/sym_exec_llvm.cpp
+++ llvm/lib/Analysis/Superopt/sym_exec_llvm.cpp
@@ -3663,8 +3663,9 @@ sym_exec_common::get_tfg_common(tfg &t)
     ss << LLVM_METHOD_ARG_PREFIX << a.first;
     string argname = ss.str();
 
+    allocsite_t allocsite = m_ctx->is_vararg_local_expr(a.second) ? graph_locals_map_t::vararg_local_id()
+                                                                  : allocsite_t::allocsite_arg(a.first);
     ss.str("");
-    allocsite_t allocsite = allocsite_t::allocsite_arg(a.first);
     ss << string(G_INPUT_KEYWORD ".") << m_srcdst_keyword << "." << G_LOCAL_KEYWORD << "." << allocsite.allocsite_to_string();
     expr_ref arg_addr = m_ctx->mk_var(ss.str(), m_ctx->get_addr_sort());
     arg_exprs.insert(make_pair(mk_string_ref(argname), graph_arg_t(arg_addr, a.second)));
